f316cad1148a1d278d976cb9fdbb413ce135bf68
Patricia Formiga <patriciaformigacontreiras@gmail.com>
2023-07-05 18:04:02 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const button = document.querySelector("button");
 const h1 = document.querySelector("h1");
+
 button.addEventListener("click", function () {
   const newColor = randomColor();
   document.body.style.backgroundColor = newColor;
