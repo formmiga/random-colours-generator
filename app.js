9f218ee6082cc6217f0dffde91682ea4183c4690
Patricia Formiga <patriciaformigacontreiras@gmail.com>
2023-07-05 17:52:03 +0100

    Update app.js
    Built the function by selecting the background colour and setting it to turn black when clicking on the button. Tested: ok.
    
    Next is generating a random colour upon clicking that button. Creating three other variables, one called R, another G, another B, they will each use the Math.floor method, then the Math.random method multiplied by 256 to generate a number between 0 and 256, to each variable. Created a new variable called newColor and set the background colour to be newColor instead.

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const button = document.querySelector("button");
 button.addEventListener("click", function () {
-  console.log("clicked dude");
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  const newColor = `rgb(${r}, ${g}, ${b})`;
+  document.body.style.backgroundColor = newColor;
 });
